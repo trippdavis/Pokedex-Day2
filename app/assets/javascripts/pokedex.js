43b0e7aa4dc79ac4bbf08c0881a6e475b1b07eaa
Ned Ruggeri <ruggeri@self-loop.com>
2014-11-03 17:59:04 -0800

    checkin some comments

--- app/assets/javascripts/pokedex.js
+++ app/assets/javascripts/pokedex.js
@@ -1,9 +1,14 @@
-window.Pokedex = function() {
+// Comments:
+// * Pass in an el.
+// * I removed comparator.
+// * Fix weird rendering bug.
+
+window.Pokedex = function () {
   this.pokes = new Pokedex.Collections.Pokemon;
 	this.$pokeList = $('.poke-list');
 	this.$pokeDetail = $('.poke-detail');
-	
-	this.$pokeList.on('click', 'li', this.showDetail.bind(this));	
+
+	this.$pokeList.on('click', 'li', this.showDetail.bind(this));
 }
 
 Pokedex.Models = {};
@@ -17,72 +22,69 @@ Pokedex.Models.Pokemon = Backbone.Model.extend({
 // create Pokemon Backbone collection
 Pokedex.Collections.Pokemon = Backbone.Collection.extend({
   model: Pokedex.Models.Pokemon,
-	url: '/pokemon',
-	comparator: 'number'
+	url: '/pokemon'
 });
 
-Pokedex.prototype.listPokemon = function (pokes, callback) {
+Pokedex.prototype.listPokemon = function (callback) {
 	// create collection
 	// fetch collection
 	// print names asynch
-  this.pokes = pokes || this.pokes || new Pokedex.Collections.Pokemon();
-  var that = this;
   this.pokes.fetch({
-  	success: function() {
-  		that.pokes.each(function(poke) {
-  			that.renderListItem(poke);
-        callback && callback();
-  		});
-  	}
+  	success: (function () {
+  		this.pokes.each(this.renderListItem.bind(this));
+      callback && callback();
+  	}).bind(this)
   });
   return this.pokes;
-}
+};
 
 Pokedex.prototype.renderListItem = function (pokemon) {
 	// build LI
 	// apped it to $pokeList
-	var $li = $('<li class="poke-list-item" data-id=' + 
+	var $li = $('<li class="poke-list-item" data-id=' +
 				pokemon.get('id') + '>');
 	var shortInfo = ['name', 'number', 'poke_type'];
-	shortInfo.forEach(function(attr) {
+	shortInfo.forEach(function (attr) {
 		$li.append(attr + ': ' + pokemon.get(attr) + '<br>');
 	});
 
 	this.$pokeList.append($li);
-}
+};
 
 Pokedex.prototype.createPokemon = function (attrs, callback) {
 	// instantiate object
 	// set attributes
 	// save and call callback
 	var poke = new Pokedex.Models.Pokemon(attrs);
+
+  // Have an alert pop-up which confirms saving when that is complete.
+  // Don't add the new pokemon until it is saved properly.
 	poke.save();
 
-  this.pokes = this.pokes || new Pokedex.Collections.Pokemon();
 	this.pokes.add(poke);
 	callback && callback.call(this, poke);
   // then use this.pokes.create({
   // success: callback
   // })
   return poke;
-}
+};
 
 Pokedex.prototype.renderDetail = function (pokemon) {
 	var num = JSON.stringify(pokemon.get('number'));
 	while(num.length < 3) {
     num = '0' + num;
 	}
-	var string = '<img src="assets/pokemon_snaps/' + num + '.png"' + 
+	var string = '<img src="assets/pokemon_snaps/' + num + '.png"' +
 				'style="float:left;"><br>';
 	for(var attr in pokemon.attributes) {
 		if(pokemon.get(attr) && attr !== 'id') {
-			string += '<span style="font-weight:bold;">' + attr + ':</span> ' + 
+			string += '<span style="font-weight:bold;">' + attr + ':</span> ' +
 						pokemon.get(attr) + '<br>';
 		}
 	}
 
 	this.$pokeDetail.html(string);
-}
+};
 
 Pokedex.prototype.showDetail = function (event) {
   var $target = $(event.target);
@@ -91,9 +93,9 @@ Pokedex.prototype.showDetail = function (event) {
 	var pokemon = this.pokes.get(pokeId);
 
 	this.renderDetail(pokemon);
-}
+};
 
 $(function() {
-	pokedex = new Pokedex;
+	var pokedex = new Pokedex();
   pokedex.listPokemon();
 });
--- app/views/pokemon/_pokemon.json.jbuilder
+++ app/views/pokemon/_pokemon.json.jbuilder
@@ -1,3 +1,3 @@
-json.extract!(pokemon, :id, :name, :number, :poke_type, :attack, 
-			:defense, :evolve_level, :evolve_to, :moves, :levels, 
-			:curve, :probability) 
+json.extract!(pokemon, :id, :name, :number, :poke_type, :attack,
+			:defense, :evolve_level, :evolve_to, :moves, :levels,
+			:curve, :probability)
