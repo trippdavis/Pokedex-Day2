9d5b83114b43569ff21759d9cc0327892b2a2d00
Ned Ruggeri <ruggeri@self-loop.com>
2014-11-16 15:14:35 -0800

    format routes file

--- solution/config/routes.rb
+++ solution/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-	root to: 'static_pages#root'
+  root to: 'static_pages#root'
 
-	resources :pokemon, defaults: {format: :json}, 
-				only: [:create, :show, :index, :destroy, :update]
+  resources(
+    :pokemon,
+    defaults: {format: :json},
+    only: [:create, :show, :index, :destroy, :update]
+  )
 end
