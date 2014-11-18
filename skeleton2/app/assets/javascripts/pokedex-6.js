6368bf64482b398ef4ea235b8f7cd28c0463bb4b
Ned Ruggeri <ruggeri@self-loop.com>
2014-11-17 23:09:42 -0800

    add skeleton2

---
+++ skeleton2/app/assets/javascripts/pokedex-6.js
@@ -0,0 +1,20 @@
+Pokedex.Router = Backbone.Router.extend({
+  routes: {
+  },
+
+  pokemonDetail: function (id, callback) {
+  },
+
+  pokemonIndex: function (callback) {
+  },
+
+  toyDetail: function (pokemonId, toyId) {
+  }
+});
+
+/*
+$(function () {
+  new Pokedex.Router();
+  Backbone.history.start();
+});
+*/
